46438b8caea47306c6841f5ddc6e8d481e250d53
Extakuma <soccer.takuma.1116@icloud.com>
2021-09-14 07:40:18 +0000

    [start]4-2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'a0502123c474b7c6be06b46f1e63f1decf4d5ea1a67b07267abe297a791f02c622215fa861e58e23da5cff7a3c0d5a2dd25563c94fb3b5561c321c949ba5ddd5'
